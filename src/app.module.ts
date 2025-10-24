eb62297ec65d52cb84c8f51c7d04fe2d44dfd2bd
Jovana <jjovana314@gmail.com>
2025-10-24 11:52:48 +0200

    added database url for connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'src/config/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, MongooseModule.forRoot(config.get().DATABASE_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
