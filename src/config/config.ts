bde9123a94a2c5c2e8d359640a7b6035e2d14be7
Jovana <jjovana314@gmail.com>
2025-10-24 12:04:23 +0200

    fixed connecting to database

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,17 @@
 import { createProfiguration } from '@golevelup/profiguration';
 import { Config } from './config.interface';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 export const config = createProfiguration<Config>(
     {
         DATABASE_URL: {
             default: '',
             env: 'DATABASE_URL',
-        }
-    })
+        },
+
+    },
+    { strict: true, verbose: true },
+);
+
