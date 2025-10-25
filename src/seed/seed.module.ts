cdaabe5d64b09ccf810e62568d7c65139f702275
Jovana <jjovana314@gmail.com>
2025-10-25 21:38:02 +0200

    updated seed module

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { GroceryModule } from '../grocery/grocery.module';
+import { UsersModule } from '../users/users.module';
+import { SeedService } from './seed.service';
 
-@Module({})
+@Module({
+  imports: [
+    GroceryModule,
+    UsersModule
+  ],
+  providers: [SeedService],
+})
 export class SeedModule {}
+
